38c3b0776202b3e3d1d72f82d9248c27ee6ee5bd
Adameq <geerafix@gmail.com>
2024-08-12 09:10:29 +0200

    feat: user profile view with card

---
+++ src/components/user/profile/UserProfile.vue
@@ -0,0 +1,103 @@
+<script setup>
+defineProps(['user']);
+</script>
+
+<template>
+    <div class="user-profile-container">
+        <div class="grid gap-2 p-2 grid-rows-[min-content_auto] text-center bg-gray-700 rounded-xl">
+            <div class="grid grid-cols-[1fr_1fr] gap-2">
+                <Icon icon="fa-solid fa-user" class="my-auto mx-auto text-5xl bg-gray-800/50 w-full py-2 rounded-lg" />
+                <div class="bg-gray-800/50 rounded-lg content-center">UID: {{ user.user_id }}</div>
+            </div>
+            <div>
+                <textarea disabled class="bio">{{ user.bio }}</textarea>
+            </div>
+        </div>
+        <div class="numbers">
+            <div class="item-container">Rozwiązane gry</div>
+                <div class="item">{{ user.solved_puzzles }}</div>
+            <div class="item-container">Punktów łącznie</div>  
+                <div class="item">{{ user.total_points }}</div>
+            <div class="item-container">Łączny czas gry</div>
+                <div class="item">{{ (user.total_play_time / 3600).toFixed(2) }} g.</div>
+            <div class="item-container">Data rejestracji</div>
+                <div class="item">{{ new Date(user.register_date).toLocaleDateString() }}r</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.user-profile-container {
+    @apply 
+    grid 
+    sm:grid-cols-2 
+    max-sm:grid-rows-2
+    gap-2
+    p-2 
+    w-fit 
+    h-fit 
+    bg-gray-600
+    rounded-2xl
+    border-b-4
+    text-xl
+    border-gray-700
+    select-none
+}
+.profile-icon {
+    @apply
+    flex
+    items-center
+    w-full
+    h-14
+    place-content-center
+    rounded-lg
+    text-gray-200
+    bg-gray-600
+    text-4xl
+    drop-shadow-sm
+    shadow-inner
+}
+.underlined {
+    @apply
+    my-2
+    border-b-2
+    border-gray-700/70
+}
+.numbers {
+    @apply
+    grid
+    grid-cols-2 
+    gap-2
+    p-2 
+    bg-gray-700
+    rounded-xl
+    text-right
+}
+.item-container {
+    @apply
+    text-right
+    text-nowrap
+    content-center
+}
+.item {
+    @apply
+    p-2
+    bg-gray-600
+    rounded-lg
+    text-right
+    drop-shadow-sm
+    shadow-inner;
+}
+.bio {
+    @apply
+    bg-gray-800/50  
+    w-full
+    h-full
+    p-2
+    rounded-lg
+    text-wrap
+    resize-none
+    outline-none
+    
+}
+</style>
