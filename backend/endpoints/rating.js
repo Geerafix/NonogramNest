aa0c33ca51fdea2bb23780d03cd22bfb836aabad
Adameq <geerafix@gmail.com>
2024-09-15 20:49:51 +0200

    feat: sorting rating by total points

--- backend/endpoints/rating.js
+++ backend/endpoints/rating.js
@@ -16,7 +16,7 @@ server.get('/rating/classic', async (req, res) => {
             attributes: [ 'username' ],
             include: {
                 model: Rating,
-                attributes: size ? [ column ] : [ 'sum' ]
+                attributes: size ? [ column ] : [ 'sum' ],
             },
             limit: limit,
             offset: offset
@@ -24,7 +24,7 @@ server.get('/rating/classic', async (req, res) => {
 
         rating = Array.from(JSON.parse(JSON.stringify(rating))).map((el) => ({ 
             username: Object.values(el)[0], totalPoints: Object.values(el.Rating)[0]
-        }));
+        })).sort((a, b) => a.totalPoints > b.totalPoints ? -1 : 1);
 
         res.json(rating);
     } catch (error) {
