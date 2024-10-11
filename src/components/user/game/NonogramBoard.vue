40fe52cb408f04fe57c2cbb258b067dd2276ad76
Adam Grzeszczuk <103129308+Geerafix@users.noreply.github.com>
2024-10-11 20:13:30 +0200

    feat: board can be painted by dragging

--- src/components/user/game/NonogramBoard.vue
+++ src/components/user/game/NonogramBoard.vue
@@ -1,18 +1,37 @@
 <script setup>
-defineProps([
+import {set, useMousePressed} from "@vueuse/core";
+import {ref} from "vue";
+
+const props = defineProps([
   'answers',
   'size',
   'paint'
 ]);
+
+const {pressed} = useMousePressed();
+const firstColour = ref();
+
+const paintHover = (col, row) => {
+  if (pressed.value && props.answers[col][row] === firstColour.value) {
+    props.paint(col, row);
+  }
+};
+
+const paintClick = (col, row) => {
+  set(firstColour, props.answers[col][row]);
+  props.paint(col, row);
+};
+
 </script>
 
 <template>
   <main class="board">
-    <div class="rows" v-for="row in size">
-      <div v-for="col in size"
-           :class="['cols', (answers[row-1] && answers[col-1][row-1] === -1) ? 'bg-gray-200/80 pointer-events-none' :
-                                 ((answers[row-1] && answers[col-1][row-1] === 1) ? 'bg-gray-800' : 'bg-white')]"
-           @mousedown.left="paint(col - 1, row - 1)"
+    <div class="rows" v-for="row in props.size">
+      <div v-for="col in props.size"
+           :class="['cols', (props.answers[row-1] && props.answers[col-1][row-1] === -1) ? 'bg-gray-200/80 pointer-events-none' :
+                                 ((props.answers[row-1] && props.answers[col-1][row-1] === 1) ? 'bg-gray-800' : 'bg-white')]"
+           @mousedown.prevent="paintClick(col - 1, row - 1)"
+           @mouseover.prevent="paintHover(col - 1, row - 1)"
            @contextmenu.prevent="">
       </div>
     </div>
